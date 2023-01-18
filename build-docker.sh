faca9b8d7e4897c405afe101b7e1e5e8125454eb
Patrik Almqvist <patrik.almqvist@maxar.com>
2023-01-18 11:38:33 +0100

    New patch set and minor fixes

--- build-docker.sh
+++ build-docker.sh
@@ -3,6 +3,6 @@
 rpm -i https://download.fedoraproject.org/pub/epel/7/SRPMS/Packages/s/slurm-20.11.9-1.el7.src.rpm
 
 patch ~/rpmbuild/SPECS/slurm.spec spec.patch
-cp db_*.patch ~/rpmbuild/SOURCES
+cp slurm-20.11.9-0.0.38-backport.patch ~/rpmbuild/SOURCES
 
 rpmbuild -bb ~/rpmbuild/SPECS/slurm.spec
